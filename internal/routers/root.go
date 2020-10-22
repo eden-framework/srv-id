45182d33c56693a0fe209b8019b1df9f6893a0dc
liyiwen <johnnyeven@gmail.com>
2020-10-22 16:01:44 +0800

    test ci

--- internal/routers/root.go
+++ internal/routers/root.go
@@ -1,17 +1,19 @@
 package routers
 
 import (
+	v0 "gitee.com/newtengroup/srv-id/internal/routers/v0"
+	"github.com/eden-framework/context"
 	"github.com/eden-framework/courier"
 	"github.com/eden-framework/courier/swagger"
-
-	v0 "gitee.com/newtengroup/srv-id/internal/routers/v0"
 )
 
 var Router = courier.NewRouter(RootRouter{})
 
 func init() {
 	Router.Register(v0.Router)
-	Router.Register(swagger.SwaggerRouter)
+	if !context.IsOnline() {
+		Router.Register(swagger.SwaggerRouter)
+	}
 }
 
 type RootRouter struct {
